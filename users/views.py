3095798f2df124e2b31e8ec29a8b290d242be331
justscoundrel <=>
2022-08-31 02:03:50 +0300

    Add ckeditor6

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView, UpdateView
@@ -6,16 +8,18 @@ from users.forms import CreationForm, ProfileForm
 from .models import Profile
 
 
-class SignUp(CreateView):
+class SignUp(SuccessMessageMixin, CreateView):
     form_class = CreationForm
     success_url = reverse_lazy("core:home")
+    success_message = 'Вы успешно авторизовались, пожалуйста заполните профиль в личном кабинете.'
     template_name = "users/signup.html"
 
 
-class ProfileCreate(CreateView):
+class ProfileCreate(SuccessMessageMixin, CreateView):
     model = Profile
     form_class = ProfileForm
     template_name = 'users/profile.html'
+    success_message = 'Профиль успешно создан, после оплаты Вам будет доступен весь функционал.'
     success_url = reverse_lazy('core:home')
 
     def post(self, request):
@@ -24,16 +28,15 @@ class ProfileCreate(CreateView):
             profile = form.save(commit=False)
             profile.user = request.user
             profile.save()
-            return redirect('users:profile_update')
-
         context = {'form': form}
         return render(request, self.template_name, context)
 
 
-class ProfileUpdate(UpdateView):
+class ProfileUpdate(SuccessMessageMixin, UpdateView):
     model = Profile
     form_class = ProfileForm
     template_name = 'users/profile_update.html'
+    success_message = 'Профиль успешно обновлен!'
     success_url = reverse_lazy('core:admin_review')
 
     def get_object(self, queryset=None):
